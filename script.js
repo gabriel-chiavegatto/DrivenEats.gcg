5ff2868dbade54708f6a72f21485787fa9b31290
Gabriel Chiavegatto Gomes <chiavegattogomes@gmail.com>
2022-01-31 13:08:26 -0300

    versao começando a selecionar as opções

---
+++ script.js
@@ -0,0 +1,20 @@
+//prato
+function selecionarPrato(botao, pratoN) {
+    
+    botao.classList.add("borda-opcao-selecionada");
+
+}
+//bebida
+function selecionarBebida(botao, bebidaN) {
+
+    botao.classList.add("borda-opcao-selecionada");
+}
+//sobremesa
+function selecionarSobremesa(botao, sobremesaN) {
+
+    botao.classList.add("borda-opcao-selecionada");
+}
+
+function fecharPedido() {
+
+}
