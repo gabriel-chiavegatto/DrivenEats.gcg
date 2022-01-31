e24e7acc549a79e63ef17f94a9bfec2a8e5768ab
Gabriel Chiavegatto Gomes <chiavegattogomes@gmail.com>
2022-01-31 15:09:41 -0300

    Seleçoes independentes

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 //prato
 function selecionarPrato(botao, pratoN) {
-    
-    botao.classList.add("borda-opcao-selecionada");
+    deselecionarPrato();
+    botao.classList.add("prato-selecionado");
 
+}
+function deselecionarPrato(){
+    const pratoSelecionado = document.querySelector(".prato-selecionado");
+    if (pratoSelecionado !== null){
+        pratoSelecionado.classList.remove("prato-selecionado");
+    }
 }
 //bebida
 function selecionarBebida(botao, bebidaN) {
-
-    botao.classList.add("borda-opcao-selecionada");
+    deselecionarBebida();
+    botao.classList.add("bebida-selecionada");
+}
+function deselecionarBebida(){
+    const bebidaSelecionada = document.querySelector(".bebida-selecionada");
+    if (bebidaSelecionada !== null){
+        bebidaSelecionada.classList.remove("bebida-selecionada");
+    }
 }
 //sobremesa
 function selecionarSobremesa(botao, sobremesaN) {
-
-    botao.classList.add("borda-opcao-selecionada");
+    deselecionarSobremesa();
+    botao.classList.add("sobremesa-selecionada");
+}
+function deselecionarSobremesa(){
+    
+    const sobremesaSelecionada = document.querySelector(".sobremesa-selecionada");
+    if (sobremesaSelecionada !== null){
+        sobremesaSelecionada.classList.remove("sobremesa-selecionada");
+    
+    }
 }
 
+//botao de fechar pedido
 function fecharPedido() {
 
 }
